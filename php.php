7893515713ce14841d5796fc1d70ae1691612e09
SevenPik <108593577+SevenPik@users.noreply.github.com>
2023-05-31 15:32:56 +0200

    Update php.php

--- php.php
+++ php.php
@@ -1,55 +1,101 @@
+
 <?php
+
 $host="localhost";
 $user="root";
 $password="";
-$db="myDB";
+$db="user";
+
+session_start();
+
+
 $data=mysqli_connect($host,$user,$password,$db);
-if($data=false){
-    die("connection error");
+
+if($data===false)
+{
+	die("connection error");
 }
+
+
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+	$username=$_POST["username"];
+	$password=$_POST["password"];
+
+
+	$sql="select * from login where username='".$username."' AND password='".$password."' ";
+
+	$result=mysqli_query($data,$sql);
+
+	$row=mysqli_fetch_array($result);
+
+	if($row["usertype"]=="user")
+	{	
+
+		$_SESSION["username"]=$username;
+
+		header("location:userhome.php");
+	}
+
+	elseif($row["usertype"]=="admin")
+	{
+
+		$_SESSION["username"]=$username;
+		
+		header("location:adminhome.php");
+	}
+
+	else
+	{
+		echo "username or password incorrect";
+	}
+
+}
+
+
+
+
 ?>
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
-    </head>
-        <?php
-        ?>
-        <body class="body">
-            <container>
-                <div class="box">
-                <div class="Tytuł">
-                    Polskie Koleje Państwowe
-                </div>
-            </container>
-            <container>
-                <div class="GłównaStrona">
-                    <div class="zawartosc">
-                
-                    <form type="text" name="login" action="#" method="POST" >Login: <input type="text" required></input></form>
-                    <form action="welcome.php" method="post">
-                        Name: <input type="text" name="name"><br>
-                        E-mail: <input type="text" name="email"><br>
-                        <input type="submit">
-                        </form>
-                    </div>
+<head>
+	<title></title>
+</head>
+<body>
 
-                </div>
-            </container>
-            <div class="divider">
-            </div>
-        </div>
-        </body>
-</html> 
+<center>
+
+	<h1>Login Form</h1>
+	<br><br><br><br>
+	<div style="background-color: grey; width: 500px;">
+		<br><br>
+
+
+		<form action="#" method="POST">
+
+	<div>
+		<label>username</label>
+		<input type="text" name="username" required>
+	</div>
+	<br><br>
+
+	<div>
+		<label>password</label>
+		<input type="password" name="password" required>
+	</div>
+	<br><br>
+
+	<div>
+		
+		<input type="submit" value="Login">
+	</div>
+
+
+	</form>
+
+
+	<br><br>
+ </div>
+</center>
+
+</body>
