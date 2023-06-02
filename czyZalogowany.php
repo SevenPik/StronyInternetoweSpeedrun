361c560b3f9618876bb32c6073693c3515c585ba
SevenPik <108593577+SevenPik@users.noreply.github.com>
2023-06-02 20:25:41 +0200

    Naprawiona Baza danych
    Nozdrzyk wykreślił

--- czyZalogowany.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION["zalogowany"])){
-	echo "jestes zalogowany jako: ".$_SESSION["login"];
-	echo "<br> <a href='logout.php'>Wyloguj sie</a>";
-}else{
-	echo "Musisz sie zalogowac: <a href='logowanie.php'>Zaloguj sie</a>";
-}
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-echo "wylogowales sie<br>";
-echo " -> <a href='czyZalogowany.php'>Sprawdz</a>";
-?>
